939d0f85fdf561759898980c2702629730522193
PlanetM <ermcgrew@users.noreply.github.com>
2022-01-12 20:57:15 -0500

    work from wednesday night

--- leaflet_maps.js
+++ leaflet_maps.js
@@ -12,41 +12,53 @@ async function originMap () {
     const response = await fetch("countries.geojson"); 
     const data = await response.json();
 
+
+    // //load country to region csv
+    // const response1 = await fetch("UNSD_country_to_regions.csv"); 
+    // const data1 = await response1.json();
+
+    // console.log(data1);
+
     //import plant origin data 
-    // const response = await fetch(""); //from where?
-    // const data = await response.json();
+    // const response = await fetch(""); //****** */
+    // const data   = await response.json();
     //*************************************************** */
     //OR pass function the dictionary/data package?
 
 
     //*************************************************** */
     //transform data as needed to have country/countries of origin listed
+    //include exception for any weird origin names
 
+    //test array of countries
     const plant = ['Bangladesh', 'Bhutan', 'India', 'Maldives', 'Nepal', 'Pakistan', 'Sri Lanka'];
 
-    let geojsonLayer = L.geoJSON()
+    //declare empty geojson layer
+    let geojsonLayer = L.geoJSON();
 
+    //declare border style parameters
     let myStyle = {
         color: "black", 
         fillOpacity: 0
     };
 
-    // Loop through plant origin countries
+    // Loop through plant origin data
     for (let index = 0; index < plant.length; index++) {
         
-        //match plant info with correct country
+        //match plant origin with correct country
         //data.features[i].properties.ADMIN //country name
-        if (plant[index] === data.features[index].properties.ADMIN) {
-            //*************************************************** */
-            //add country polygon to map
+        if (plant[index] === data.features[index].properties.ADMIN) {///this is wrong
+            
+            //add country geojson to layer
             geojsonLayer.addData(data.features[index], {style: myStyle});      
         };
     };
 
+
     // Create the map object
     let myMap = L.map("map", {
-    center: [40.73, -74.0059], //************** */
-    zoom: 5,
+    center: [35.9375, 14.3754],
+    zoom: 3,
     layers: [geojsonLayer] 
     });
 
