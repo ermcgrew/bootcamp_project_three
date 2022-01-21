f4ab71745ab36a03de3c335529be7f2a3571e274
JennyShea <86893003+JennyShea@users.noreply.github.com>
2022-01-21 17:09:23 -0500

    toxic to pets info

---
+++ toxic_to_pets/toxic.js
@@ -0,0 +1,719 @@
+[
+    {
+      : "0",
+      Common Name: "Amaryllis",
+      toxic_cats: "Yes",
+      toxic_dogs: "Yes",
+      not_toxic: ""
+    },
+    {
+      : "1",
+      Common Name: "African violet",
+      toxic_cats: "no",
+      toxic_dogs: "no",
+      not_toxic: "yes"
+    },
+    {
+      : "2",
+      Common Name: "Angel wing begonia",
+      toxic_cats: "Yes",
+      toxic_dogs: "Yes",
+      not_toxic: ""
+    },
+    {
+      : "3",
+      Common Name: "Barberton, Gerbera, Transvaal Daisy",
+      toxic_cats: "no",
+      toxic_dogs: "no",
+      not_toxic: "yes"
+    },
+    {
+      : "4",
+      Common Name: "Spider Lily",
+      toxic_cats: "no",
+      toxic_dogs: "no",
+      not_toxic: "yes"
+    },
+    {
+      : "5",
+      Common Name: "Belladonna lily, March lily and naked lady.",
+      toxic_cats: "Yes",
+      toxic_dogs: "Yes",
+      not_toxic: ""
+    },
+    {
+      : "6",
+      Common Name: "Bird of Paradise, Strelitzia, Crane Flower",
+      toxic_cats: "Yes",
+      toxic_dogs: "Yes",
+      not_toxic: ""
+    },
+    {
+      : "7",
+      Common Name: "Blushing bromeliad",
+      toxic_cats: "no",
+      toxic_dogs: "no",
+      not_toxic: "yes"
+    },
+    {
+      : "8",
+      Common Name: "Busy Lizzie and Impatient Lucy",
+      toxic_cats: "no",
+      toxic_dogs: "no",
+      not_toxic: "yes"
+    },
+    {
+      : "9",
+      Common Name: "Calla lily",
+      toxic_cats: "Yes",
+      toxic_dogs: "Yes",
+      not_toxic: ""
+    },
+    {
+      : "10",
+      Common Name: "Coral berry, christmas berry, coral ardisia",
+      toxic_cats: "Unknown",
+      toxic_dogs: "Unknown",
+      not_toxic: "no"
+    },
+    {
+      : "11",
+      Common Name: "Corsage Orchid",
+      toxic_cats: "no",
+      toxic_dogs: "no",
+      not_toxic: "yes"
+    },
+    {
+      : "12",
+      Common Name: "Cylamen, florist cyclamen",
+      toxic_cats: "Yes",
+      toxic_dogs: "Yes",
+      not_toxic: ""
+    },
+    {
+      : "13",
+      Common Name: "Eternal Flame",
+      toxic_cats: "no",
+      toxic_dogs: "no",
+      not_toxic: "yes"
+    },
+    {
+      : "14",
+      Common Name: "False shamrock, purple sharock, wood sorrel",
+      toxic_cats: "Yes",
+      toxic_dogs: "Yes",
+      not_toxic: ""
+    },
+    {
+      : "15",
+      Common Name: "Flamingo flower, pigtail plant",
+      toxic_cats: "Yes",
+      toxic_dogs: "Yes",
+      not_toxic: ""
+    },
+    {
+      : "16",
+      Common Name: "Flaming Sword",
+      toxic_cats: "no",
+      toxic_dogs: "no",
+      not_toxic: "yes"
+    },
+    {
+      : "17",
+      Common Name: "F. maple,",
+      toxic_cats: "Unknown",
+      toxic_dogs: "Unknown",
+      not_toxic: "no"
+    },
+    {
+      : "18",
+      Common Name: "Kaffir and bush lily",
+      toxic_cats: "Yes",
+      toxic_dogs: "Yes",
+      not_toxic: ""
+    },
+    {
+      : "19",
+      Common Name: "Lollipop and golden shrimp plant",
+      toxic_cats: "Unknown",
+      toxic_dogs: "Unknown",
+      not_toxic: "no"
+    },
+    {
+      : "20",
+      Common Name: "Madagascar Jasmine - Wax Flower",
+      toxic_cats: "no",
+      toxic_dogs: "no",
+      not_toxic: "yes"
+    },
+    {
+      : "21",
+      Common Name: "Medusa Plant",
+      toxic_cats: "Unknown",
+      toxic_dogs: "Unknown",
+      not_toxic: "no"
+    },
+    {
+      : "22",
+      Common Name: "Christmas and ornamental pepper plant",
+      toxic_cats: "Yes",
+      toxic_dogs: "Yes",
+      not_toxic: ""
+    },
+    {
+      : "23",
+      Common Name: "Poinsettia,",
+      toxic_cats: "Yes",
+      toxic_dogs: "Yes",
+      not_toxic: ""
+    },
+    {
+      : "24",
+      Common Name: "Queens tears, friendship plant",
+      toxic_cats: "no",
+      toxic_dogs: "no",
+      not_toxic: "yes"
+    },
+    {
+      : "25",
+      Common Name: "Rose of China, Chinese hibiscus, Shoe flower",
+      toxic_cats: "Yes",
+      toxic_dogs: "Yes",
+      not_toxic: ""
+    },
+    {
+      : "26",
+      Common Name: "Scarlet star",
+      toxic_cats: "no",
+      toxic_dogs: "no",
+      not_toxic: "yes"
+    },
+    {
+      : "27",
+      Common Name: "The one colored paphiopedilum, lady's slipper",
+      toxic_cats: "Unknown",
+      toxic_dogs: "Unknown",
+      not_toxic: "no"
+    },
+    {
+      : "28",
+      Common Name: "Urn and pink or silver vase plant",
+      toxic_cats: "no",
+      toxic_dogs: "no",
+      not_toxic: "yes"
+    },
+    {
+      : "29",
+      Common Name: "Winter cherry",
+      toxic_cats: "Yes",
+      toxic_dogs: "Yes",
+      not_toxic: ""
+    },
+    {
+      : "30",
+      Common Name: "Aluminum Plant",
+      toxic_cats: "Yes",
+      toxic_dogs: "Yes",
+      not_toxic: ""
+    },
+    {
+      : "31",
+      Common Name: "Areca, butterfly, or cane palm",
+      toxic_cats: "no",
+      toxic_dogs: "no",
+      not_toxic: "yes"
+    },
+    {
+      : "32",
+      Common Name: "Arrowhead Plant or Arrowhead Vine, Goosefoot Plant",
+      toxic_cats: "Yes",
+      toxic_dogs: "Yes",
+      not_toxic: ""
+    },
+    {
+      : "33",
+      Common Name: "Bird's Nest Fern",
+      toxic_cats: "no",
+      toxic_dogs: "no",
+      not_toxic: "yes"
+    },
+    {
+      : "34",
+      Common Name: "Boston Fern",
+      toxic_cats: "no",
+      toxic_dogs: "no",
+      not_toxic: "yes"
+    },
+    {
+      : "35",
+      Common Name: "Broadleaf lady or little lady palm",
+      toxic_cats: "no",
+      toxic_dogs: "no",
+      not_toxic: "yes"
+    },
+    {
+      : "36",
+      Common Name: "Canary Island Date Palm",
+      toxic_cats: "no",
+      toxic_dogs: "no",
+      not_toxic: "yes"
+    },
+    {
+      : "37",
+      Common Name: "Cast iron plant",
+      toxic_cats: "no",
+      toxic_dogs: "no",
+      not_toxic: "yes"
+    },
+    {
+      : "38",
+      Common Name: "Chinese evergreen",
+      toxic_cats: "Yes",
+      toxic_dogs: "Yes",
+      not_toxic: ""
+    },
+    {
+      : "39",
+      Common Name: "Coral bead, pin cushion plant",
+      toxic_cats: "Yes",
+      toxic_dogs: "Yes",
+      not_toxic: ""
+    },
+    {
+      : "40",
+      Common Name: "Corn or cornstalk plant",
+      toxic_cats: "Yes",
+      toxic_dogs: "Yes",
+      not_toxic: ""
+    },
+    {
+      : "41",
+      Common Name: "Creeping or climbing fig",
+      toxic_cats: "Yes",
+      toxic_dogs: "Yes",
+      not_toxic: ""
+    },
+    {
+      : "42",
+      Common Name: "Cretan Brake Fern",
+      toxic_cats: "Unknown",
+      toxic_dogs: "Unknown",
+      not_toxic: "no"
+    },
+    {
+      : "43",
+      Common Name: "Croton, Joseph's coat",
+      toxic_cats: "Yes",
+      toxic_dogs: "Yes",
+      not_toxic: ""
+    },
+    {
+      : "44",
+      Common Name: "Dumb cane",
+      toxic_cats: "Yes",
+      toxic_dogs: "Yes",
+      not_toxic: ""
+    },
+    {
+      : "45",
+      Common Name: "Elephant's ear - Alocasia polly - African mask",
+      toxic_cats: "Yes",
+      toxic_dogs: "Yes",
+      not_toxic: ""
+    },
+    {
+      : "46",
+      Common Name: "European fan, Mediterranean dwarf palm, and Dwarf fan palm",
+      toxic_cats: "Unknown",
+      toxic_dogs: "Unknown",
+      not_toxic: "no"
+    },
+    {
+      : "47",
+      Common Name: "Fiddle leaf fig tree",
+      toxic_cats: "Yes",
+      toxic_dogs: "Yes",
+      not_toxic: ""
+    },
+    {
+      : "48",
+      Common Name: "Golden pothos or Devils ivy",
+      toxic_cats: "Yes",
+      toxic_dogs: "Yes",
+      not_toxic: ""
+    },
+    {
+      : "49",
+      Common Name: "Green Velvet Alocasia, Alocasia Frydek",
+      toxic_cats: "Yes",
+      toxic_dogs: "Yes",
+      not_toxic: ""
+    },
+    {
+      : "50",
+      Common Name: "Hawaiian ti, good luck, baby doll ti and others",
+      toxic_cats: "Yes",
+      toxic_dogs: "Yes",
+      not_toxic: ""
+    },
+    {
+      : "51",
+      Common Name: "Heart of Jesus, angel wings, elephant ear, fancy leafed caladium",
+      toxic_cats: "Yes",
+      toxic_dogs: "Yes",
+      not_toxic: ""
+    },
+    {
+      : "52",
+      Common Name: "Kentia, paradise, thatch",
+      toxic_cats: "no",
+      toxic_dogs: "no",
+      not_toxic: "yes"
+    },
+    {
+      : "53",
+      Common Name: "Lucky Bamboo",
+      toxic_cats: "Unknown",
+      toxic_dogs: "Unknown",
+      not_toxic: "no"
+    },
+    {
+      : "54",
+      Common Name: "Dragon Tree",
+      toxic_cats: "Yes",
+      toxic_dogs: "Yes",
+      not_toxic: ""
+    },
+    {
+      : "55",
+      Common Name: "Delta Maidenhair Nest Fern",
+      toxic_cats: "Unknown",
+      toxic_dogs: "Unknown",
+      not_toxic: "no"
+    },
+    {
+      : "56",
+      Common Name: "Mexican fortune tree, money tree plant and others",
+      toxic_cats: "no",
+      toxic_dogs: "no",
+      not_toxic: "yes"
+    },
+    {
+      : "57",
+      Common Name: "Parlor, Parlour palm",
+      toxic_cats: "no",
+      toxic_dogs: "no",
+      not_toxic: "yes"
+    },
+    {
+      : "58",
+      Common Name: "Pygmy date, Miniature date palm",
+      toxic_cats: "no",
+      toxic_dogs: "no",
+      not_toxic: "yes"
+    },
+    {
+      : "59",
+      Common Name: "Rabbit's Foot Fern",
+      toxic_cats: "no",
+      toxic_dogs: "no",
+      not_toxic: "yes"
+    },
+    {
+      : "60",
+      Common Name: "Rattlesnake Plant",
+      toxic_cats: "no",
+      toxic_dogs: "no",
+      not_toxic: "yes"
+    },
+    {
+      : "61",
+      Common Name: "Rose Painted Calathea",
+      toxic_cats: "no",
+      toxic_dogs: "no",
+      not_toxic: "yes"
+    },
+    {
+      : "62",
+      Common Name: "Rubber plant or tree, rubber bush",
+      toxic_cats: "Yes",
+      toxic_dogs: "Yes",
+      not_toxic: ""
+    },
+    {
+      : "63",
+      Common Name: "Sago Palm",
+      toxic_cats: "Yes",
+      toxic_dogs: "Yes",
+      not_toxic: ""
+    },
+    {
+      : "64",
+      Common Name: "Sentry, belmore sentry, and curly palm",
+      toxic_cats: "no",
+      toxic_dogs: "no",
+      not_toxic: "yes"
+    },
+    {
+      : "65",
+      Common Name: "Song of India, Pleomele",
+      toxic_cats: "Yes",
+      toxic_dogs: "Yes",
+      not_toxic: ""
+    },
+    {
+      : "66",
+      Common Name: "Spider or airplane",
+      toxic_cats: "no",
+      toxic_dogs: "no",
+      not_toxic: "yes"
+    },
+    {
+      : "67",
+      Common Name: "Swiss cheese or just cheese plant",
+      toxic_cats: "no",
+      toxic_dogs: "no",
+      not_toxic: "yes"
+    },
+    {
+      : "68",
+      Common Name: "Umbrella Plant, Queensland Umbrella Tree, Octupus Tree, Gold Capella",
+      toxic_cats: "Yes",
+      toxic_dogs: "Yes",
+      not_toxic: ""
+    },
+    {
+      : "69",
+      Common Name: "Venus fly trap",
+      toxic_cats: "no",
+      toxic_dogs: "no",
+      not_toxic: "yes"
+    },
+    {
+      : "70",
+      Common Name: "Wandering Jew, Flowering Inch Plant",
+      toxic_cats: "no",
+      toxic_dogs: "no",
+      not_toxic: "yes"
+    },
+    {
+      : "71",
+      Common Name: "Weeping Fig",
+      toxic_cats: "Yes",
+      toxic_dogs: "Yes",
+      not_toxic: ""
+    },
+    {
+      : "72",
+      Common Name: "Zebra plant",
+      toxic_cats: "no",
+      toxic_dogs: "no",
+      not_toxic: "yes"
+    },
+    {
+      : "73",
+      Common Name: "Zebra plant",
+      toxic_cats: "no",
+      toxic_dogs: "no",
+      not_toxic: "yes"
+    },
+    {
+      : "74",
+      Common Name: "ZZ plant",
+      toxic_cats: "Yes",
+      toxic_dogs: "Yes",
+      not_toxic: ""
+    },
+    {
+      : "75",
+      Common Name: "Aloe V, Medicine Plant, True Aloe, Burn Plant,",
+      toxic_cats: "Yes",
+      toxic_dogs: "Yes",
+      not_toxic: ""
+    },
+    {
+      : "76",
+      Common Name: "Not known",
+      toxic_cats: "Unknown",
+      toxic_dogs: "Unknown",
+      not_toxic: "no"
+    },
+    {
+      : "77",
+      Common Name: "Baby rubber plant, American rubber plant, Pepper face plant",
+      toxic_cats: "no",
+      toxic_dogs: "no",
+      not_toxic: "yes"
+    },
+    {
+      : "78",
+      Common Name: "Bunny ear and polka-dot cactus",
+      toxic_cats: "Yes",
+      toxic_dogs: "Yes",
+      not_toxic: ""
+    },
+    {
+      : "79",
+      Common Name: "Century plant, Agave cactus, Maguey, American aloe",
+      toxic_cats: "Yes",
+      toxic_dogs: "Yes",
+      not_toxic: ""
+    },
+    {
+      : "80",
+      Common Name: "Christmas and holiday cactus",
+      toxic_cats: "no",
+      toxic_dogs: "no",
+      not_toxic: "yes"
+    },
+    {
+      : "81",
+      Common Name: "Christmas cheer, Pork and beans",
+      toxic_cats: "Yes",
+      toxic_dogs: "Yes",
+      not_toxic: ""
+    },
+    {
+      : "82",
+      Common Name: "Coral cactus",
+      toxic_cats: "Yes",
+      toxic_dogs: "Yes",
+      not_toxic: ""
+    },
+    {
+      : "83",
+      Common Name: "Donkey's and Burros Tail",
+      toxic_cats: "no",
+      toxic_dogs: "no",
+      not_toxic: "yes"
+    },
+    {
+      : "84",
+      Common Name: "Easter or holiday cactus",
+      toxic_cats: "no",
+      toxic_dogs: "no",
+      not_toxic: "yes"
+    },
+    {
+      : "85",
+      Common Name: "Housetree Leek, Black Rose Tree",
+      toxic_cats: "Unknown",
+      toxic_dogs: "Unknown",
+      not_toxic: "no"
+    },
+    {
+      : "86",
+      Common Name: "Flaming katy",
+      toxic_cats: "Yes",
+      toxic_dogs: "Yes",
+      not_toxic: ""
+    },
+    {
+      : "87",
+      Common Name: "Goat's horn cactus",
+      toxic_cats: "Unknown",
+      toxic_dogs: "Unknown",
+      not_toxic: "no"
+    },
+    {
+      : "88",
+      Common Name: "Golden barrel cactus , golden ball cactus, mother-in-law's cushion",
+      toxic_cats: "Unknown",
+      toxic_dogs: "Unknown",
+      not_toxic: "no"
+    },
+    {
+      : "89",
+      Common Name: "Jade, money , lucky, friendship",
+      toxic_cats: "Yes",
+      toxic_dogs: "Yes",
+      not_toxic: ""
+    },
+    {
+      : "90",
+      Common Name: "Jelly beans, Many fingers",
+      toxic_cats: "Yes",
+      toxic_dogs: "Yes",
+      not_toxic: ""
+    },
+    {
+      : "91",
+      Common Name: "Mother in law's tongue and snake plant",
+      toxic_cats: "Yes",
+      toxic_dogs: "Yes",
+      not_toxic: ""
+    },
+    {
+      : "92",
+      Common Name: "Orchid Cactus",
+      toxic_cats: "Unknown",
+      toxic_dogs: "Unknown",
+      not_toxic: "no"
+    },
+    {
+      : "93",
+      Common Name: "Panda plant or pussy ears",
+      toxic_cats: "Yes",
+      toxic_dogs: "Yes",
+      not_toxic: ""
+    },
+    {
+      : "94",
+      Common Name: "Parallel peperomia",
+      toxic_cats: "no",
+      toxic_dogs: "no",
+      not_toxic: "yes"
+    },
+    {
+      : "95",
+      Common Name: "Peruvian apple",
+      toxic_cats: "Unknown",
+      toxic_dogs: "Unknown",
+      not_toxic: "no"
+    },
+    {
+      : "96",
+      Common Name: "Pincusion Peperomia, Bean Plant",
+      toxic_cats: "no",
+      toxic_dogs: "no",
+      not_toxic: "yes"
+    },
+    {
+      : "97",
+      Common Name: "Rat's Tail",
+      toxic_cats: "Unknown",
+      toxic_dogs: "Unknown",
+      not_toxic: "no"
+    },
+    {
+      : "98",
+      Common Name: "Tiger Jaws",
+      toxic_cats: "Unknown",
+      toxic_dogs: "Unknown",
+      not_toxic: "no"
+    },
+    {
+      : "99",
+      Common Name: "Trailing jade, round leaf Peperomia",
+      toxic_cats: "no",
+      toxic_dogs: "no",
+      not_toxic: "yes"
+    },
+    {
+      : "100",
+      Common Name: "Truncate living stone",
+      toxic_cats: "Unknown",
+      toxic_dogs: "Unknown",
+      not_toxic: "no"
+    },
+    {
+      : "101",
+      Common Name: "Zebra Haworthia or Zebra Cactus",
+      toxic_cats: "no",
+      toxic_dogs: "no",
+      not_toxic: "yes"
+    },
+    {
+      : ""
+    }
+  ]
